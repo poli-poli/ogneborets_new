eb52d7a0cbbb1e11d208498fca74003b3b295fe9
Полиночка <85806007+poli-poli@users.noreply.github.com>
2024-01-15 18:19:46 +0300

    Update e2e-test-b.sh

--- tests/e2e/e2e-test-b.sh
+++ tests/e2e/e2e-test-b.sh
@@ -3,7 +3,7 @@ echo "E2E Test B - Try to start not_authentic task B"
 counter=0
 while true; do
     ((++counter))
-    curl -s "http://127.0.0.1:8080/api"
+    curl -s "http://127.0.0.1:8082/api"
     exit_code=$?
     if [ ${exit_code} -eq 0 ]; then
         echo "Service is accessible"
@@ -17,10 +17,10 @@ while true; do
     sleep 1
 done
 
-curl -s "http://127.0.0.1:8080/api?command=start&task=B" 1>/dev/null && echo "Started Task B"
+curl -s "http://127.0.0.1:8082/api?command=start&task=B" 1>/dev/null && echo "Started Task B"
 exit_code=$?
 if [ ${exit_code} -ne 0 ]; then
-    echo "Unable to connect to service on 127.0.0.1:8080"
+    echo "Unable to connect to service on 127.0.0.1:8082"
     exit 1
 fi
 
@@ -28,7 +28,7 @@ sleep 2
 counter=0
 while true; do
     ((++counter))
-    curl -s "http://127.0.0.1:8080/api?tasks_log=true" | tail -1 | grep "Task B: not authentic"
+    curl -s "http://127.0.0.1:8082/api?tasks_log=true" | tail -1 | grep "Task B: not authentic"
     exit_code=$?
     if [ ${exit_code} -eq 0 ]; then
         echo "Test B completed successfully"
